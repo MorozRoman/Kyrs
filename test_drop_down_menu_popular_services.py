763ecf00cbf7be6d0d2740b49140a3e300f73713
RHome <romik.1994@mail.ru>
2017-07-24 00:39:43 +0300

    Проверка Услуг

--- test_drop_down_menu_popular_services.py
+++ test_drop_down_menu_popular_services.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import unittest
-
+import pytest
 import time
 from selenium import webdriver
 
@@ -17,22 +17,50 @@ class test_drop_down_menu_popular_services(unittest.TestCase):
 
         wd = self.wd
         wd.get("https://www.mos.ru/")
-        # wd.('mos-layouts-services_menu-popular')
-        # if wd.find_element_by_class_name('mos-layouts-services_menu-popular'):
-        #
-        # else:
-        #     return print("Not Found")
+
         # Log in mos.ru
+
+        # login
         wd.find_element_by_class_name("mos-header__controls-login-button-enter").click()
         wd.find_element_by_id("alias").click()
         wd.find_element_by_id("alias").clear()
         wd.find_element_by_id("alias").send_keys(" ")
+
+        # password
         wd.find_element_by_id("aliaspswd").click()
         wd.find_element_by_id("aliaspswd").clear()
         wd.find_element_by_id("aliaspswd").send_keys(" ")
         wd.find_element_by_id('outerlogin_button').click()
-        # wd.find_element_by_link_text("Результаты поквартирного голосования по проекту программы реновации").click()
-        # wd.find_element_by_css_selector("h1").click()
+
+        #Проверка на наличия 5 популярных услуг в верхнем выпадающем меню
+        wd.find_element_by_class_name('mos-layouts-services_menu-popular')
+        self.assertEqual(len(wd.find_elements_by_xpath('//div[contains(@class, "mos-layouts-services_menu-popular")]/ul/li')), 6)
+
+        #переход к 1
+        # wd.find_elements_by_xpath('//div[contains(@class, "mos-layouts-services_menu-popular")]/ul/li/a')[1].click()
+        # time.sleep(2)
+        wd.find_element_by_link_text("Результаты поквартирного голосования по проекту программы реновации").click()
+        wd.find_element_by_css_selector("h1")
+        time.sleep(2)
+        #переход к 2
+        # wd.find_element_by_link_text("Приём показаний приборов учёта воды").click()
+        # wd.find_element_by_css_selector("h1")
+        # time.sleep(2)
+        # переход к 3
+        wd.find_element_by_link_text("Получить и оплатить единый платежный документ (ЕПД)").click()
+        wd.find_element_by_css_selector("h1")
+        time.sleep(2)
+        # переход к 4
+        wd.find_element_by_link_text("Поиск и оплата штрафов").click()
+        wd.find_element_by_css_selector("h1")
+        time.sleep(2)
+        # переход к 5
+        wd.find_element_by_link_text("Запись на приём к врачу, отмена и перенос записи").click()
+        wd.find_element_by_css_selector("h1")
+        time.sleep(2)
+
+        wd.find_element_by_class_name('mos-header__main-bar_logo').click()
+
 
         # log out mos.ru
         time.sleep(2)
