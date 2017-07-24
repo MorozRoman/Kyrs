d4d5b1cc7d62719d4b68f0def2d1380436191194
MorozRoman <morozov-r@mail.ru>
2017-07-24 22:59:10 +0300

    Переход от unittest к pytest

--- test_drop_down_menu_popular_services.py
+++ test_drop_down_menu_popular_services.py
@@ -1,85 +1,19 @@
 # -*- coding: utf-8 -*-
-import unittest
-import pytest
-import time
-from selenium import webdriver
 from account import Account
+import pytest
+from initialization import Initialization
+
+# Функция создающая фикстуру
+# requset.addfinalizer - параметр с методом разрушающий фикстуру
+@pytest.fixture()
+def app(request):
+    fixture = Initialization()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-class test_drop_down_menu_popular_services(unittest.TestCase):
-
-    def setUp(self):
-        self.wd = webdriver.Chrome()
-        self.wd.maximize_window()
-        self.wd.implicitly_wait(60)
-
-    def logout(self, wd):
-        # logout mos.ru
-        time.sleep(2)
-        wd.find_element_by_class_name('mos-layout-icon-dropdown_up').click()
-        wd.find_element_by_class_name("mos-layout-auth-out").click()
-
-    def go_to_pages(self, wd):
-        # Проверка страниц
-        # переход к 1
-        # wd.find_elements_by_xpath('//div[contains(@class, "mos-layouts-services_menu-popular")]/ul/li/a')[1].click()
-        # time.sleep(2)
-        wd.find_element_by_link_text("Результаты поквартирного голосования по проекту программы реновации").click()
-        # wd.find_element_by_css_selector("h1")
-        time.sleep(2)
-        # переход к 2
-        wd.find_element_by_link_text("Приём показаний приборов учёта воды").click()
-        # wd.find_element_by_css_selector("h1")
-        time.sleep(2)
-        # переход к 3
-        wd.find_element_by_link_text("Получить и оплатить единый платежный документ (ЕПД)").click()
-        # wd.find_element_by_css_selector("h1")
-        time.sleep(2)
-        # переход к 4
-        wd.find_element_by_link_text("Поиск и оплата штрафов").click()
-        wd.find_element_by_css_selector("h1")
-        time.sleep(2)
-        # переход к 5
-        wd.find_element_by_link_text("Запись на приём к врачу, отмена и перенос записи").click()
-        wd.find_element_by_css_selector("h1")
-        time.sleep(2)
-
-    def present_popular_services(self, wd):
-        # Проверка на наличия 5 популярных услуг в верхнем выпадающем меню
-        wd.find_element_by_class_name('mos-layouts-services_menu-popular')
-        self.assertEqual(
-            len(wd.find_elements_by_xpath('//div[contains(@class, "mos-layouts-services_menu-popular")]/ul/li/a')), 6)
-
-    def login(self, wd, account):
-        # login
-        wd.find_element_by_class_name("mos-header__controls-login-button-enter").click()
-        wd.find_element_by_id("alias").click()
-        wd.find_element_by_id("alias").clear()
-        wd.find_element_by_id("alias").send_keys(account.username)
-        # password
-        wd.find_element_by_id("aliaspswd").click()
-        wd.find_element_by_id("aliaspswd").clear()
-        wd.find_element_by_id("aliaspswd").send_keys(account.password)
-        wd.find_element_by_id('outerlogin_button').click()
-
-    def open_home_page(self, wd):
-        wd.get("https://www.mos.ru/")
-
-
-    def test_Drop_down_menu_popular_services(self):
-
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, Account(username=" ", password=" "))
-        self.present_popular_services(wd)
-        self.go_to_pages(wd)
-        self.logout(wd)
-
-
-
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+def test_Drop_down_menu_popular_services(app):
+    app.login(Account(username=" ", password=" "))
+    app.present_popular_services()
+    app.go_to_pages()
+    app.logout()
