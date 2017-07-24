848ccb02717384907f51876f9062a05885fa27b4
MorozRoman <morozov-r@mail.ru>
2017-07-24 12:18:07 +0300

    Передача параметров в методы

--- test_drop_down_menu_popular_services.py
+++ test_drop_down_menu_popular_services.py
@@ -50,16 +50,16 @@ class test_drop_down_menu_popular_services(unittest.TestCase):
         self.assertEqual(
             len(wd.find_elements_by_xpath('//div[contains(@class, "mos-layouts-services_menu-popular")]/ul/li/a')), 6)
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         # login
         wd.find_element_by_class_name("mos-header__controls-login-button-enter").click()
         wd.find_element_by_id("alias").click()
         wd.find_element_by_id("alias").clear()
-        wd.find_element_by_id("alias").send_keys("")
+        wd.find_element_by_id("alias").send_keys(username)
         # password
         wd.find_element_by_id("aliaspswd").click()
         wd.find_element_by_id("aliaspswd").clear()
-        wd.find_element_by_id("aliaspswd").send_keys("")
+        wd.find_element_by_id("aliaspswd").send_keys(password)
         wd.find_element_by_id('outerlogin_button').click()
 
     def open_home_page(self, wd):
@@ -70,7 +70,7 @@ class test_drop_down_menu_popular_services(unittest.TestCase):
 
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, " ", " ")
         self.present_popular_services(wd)
         self.go_to_pages(wd)
         self.logout(wd)
