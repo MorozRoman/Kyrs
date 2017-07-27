6a00c423b52f920ebd81b31713431405d1b1836a
MorozRoman <morozov-r@mail.ru>
2017-07-27 12:51:22 +0300

    Модуль: создание интелектуального Login и Logout

--- conftest.py
+++ conftest.py
@@ -23,19 +23,30 @@ def app(request):
     global fixture
     if fixture is None:
         fixture = Initialization()
-        fixture.session.login(Account(username=" ", password=" "))
+        # fixture.session.login(Account(username=" ", password=" "))
     else:
         if not fixture.is_valid():
             fixture = Initialization()
-            fixture.session.login(Account(username=" ", password=" "))
+            # fixture.session.login(Account(username=" ", password=" "))
+    fixture.session.ensure_login(Account(username=" ", password=" "))
     return fixture
 
 
 # autouse=True - позволяет выполнятся stop автоматически без указания в каком либо методе
+# @pytest.fixture(scope="session", autouse=True)
+# def stop(request):
+#     def fin():
+#         fixture.session.logout()
+#         fixture.destroy()
+#     request.addfinalizer(fin)
+#     return fixture
+
+
+#Модуль: интелектуальный Login и Logout
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def fin():
-        fixture.session.logout()
+        fixture.session.ensure_logout()
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
